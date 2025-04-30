3b2405afeaabfbed51975d99903e7f3e8722908d
allclonerrepo <tfwmailling01@gmail.com>
2025-04-30 16:38:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID","27383453"))
 API_HASH = getenv("API_HASH","4c246fb0c649477cc2e79b6a178ddfaa")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID","6961211249"))
+OWNER_ID = int(getenv("OWNER_ID","7507408570"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN","THE_FUCKER_BOTS_2926")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://moonwalahehe:moonwalahehe100@moowalahehe.hwlx8gx.mongodb.net/")
+MUST_JOIN = getenv("MUST_JOIN","abt_innocent")
